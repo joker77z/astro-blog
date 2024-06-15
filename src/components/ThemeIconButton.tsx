36860cf46584ee01d920b84e371fd96927ccddc2
TaejoonPark <joker77z@naver.com>
2024-06-16 01:23:30 +0900

    feat: 다크모드 전환 고도화
    - 다크모드로 시작해야 할 때 새로고침 시 흰색화면부터 렌더링되지 않도록 해결
    - ThemeIconButotn은 리액트로 만들어서 내부에서 상태에 따라 아이콘이 전환되도록 컨트롤 함
    
    앞으로 시도할 것 : 리액트를 완전 사용하지 않을 수 있도록 외부로 2개 다 꺼내고 css로 잘하면 해결가능할듯

---
+++ src/components/ThemeIconButton.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import MoonSVG from "../assets/Moon";
+import SunSVG from "../assets/Sun";
+import Styles from "./ThemeIconButton.module.css";
+
+export default function ThemeIconButton() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const $themeChangeBtn = document.querySelector("#themeChangeBtn");
+
+    $themeChangeBtn?.addEventListener("click", () => {
+      // dark가 없으면 추가하고 true, dark가 이미 있으면 제거하고 false
+      const isDark = document.documentElement.classList.toggle("dark");
+      localStorage.setItem("theme", isDark ? "dark" : "light");
+      if (isDark) {
+        $themeChangeBtn?.classList.add("dark");
+        setDarkMode(true);
+      } else {
+        $themeChangeBtn?.classList.remove("dark");
+        setDarkMode(false);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    const isDarkMode = theme === "dark" || (!theme && window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+    const $themeChangeBtn = document.querySelector("#themeChangeBtn");
+
+    if (isDarkMode || darkMode) {
+      document.documentElement.classList.add("dark");
+    }
+    if (isDarkMode) {
+      $themeChangeBtn?.classList.add("dark");
+      setDarkMode(true);
+    } else {
+      $themeChangeBtn?.classList.remove("dark");
+      setDarkMode(false);
+    }
+  }, [darkMode]);
+
+  return (
+    <button id="themeChangeBtn" className={Styles["theme-change-btn"]}>
+      {darkMode ? <MoonSVG /> : <SunSVG />}
+    </button>
+  );
+}
